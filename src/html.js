4dc000db9cee4033cb7af46c4ef08f594c3e40d1
Cyberons <platon.pustovoyt@gmail.com>
2024-03-14 03:46:50 +0200

    html

---
+++ src/html.js
@@ -0,0 +1,13 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+// import Accordion js
+import Accordion from 'accordion-js';
+// import Accordion styles
+import 'accordion-js/dist/accordion.min.css';
