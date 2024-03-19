f161bef1f61e448469747b7dd4ab8767c1a0e852
Bohdan2k23 <130351031+Bohdan2k23@users.noreply.github.com>
2024-03-19 08:58:17 +0100

    review css

--- src/js/api.js
+++ src/js/api.js
@@ -1,14 +1,14 @@
-import 'swiper/css'
+import 'swiper/css';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-
-
 async function fetchData() {
   try {
-    const response = await fetch("https://portfolio-js.b.goit.study/api/reviews");
+    const response = await fetch(
+      'https://portfolio-js.b.goit.study/api/reviews'
+    );
     if (!response.ok) {
       throw new Error('Network response was not OK!');
     }
@@ -17,9 +17,9 @@ async function fetchData() {
     console.error('Error fetching data:', error);
     throw error;
   }
-};
+}
 
-const swiperWrapper = document.querySelector(".swiper-list-container");
+const swiperWrapper = document.querySelector('.swiper-list-container');
 
 function createSlide(review) {
   return `<li class="swiper-slide swiper-review-slide">
@@ -33,14 +33,14 @@ function createSlide(review) {
 
 function renderSlides(reviewsData) {
   if (reviewsData.length === 0) {
-    alert("Not Found")
+    alert('Not Found');
   } else {
-    const slidesHTML = reviewsData.map(review => createSlide(review)).join("");
+    const slidesHTML = reviewsData.map(review => createSlide(review)).join('');
     swiperWrapper.insertAdjacentHTML('beforeend', slidesHTML);
   }
 }
 
- document.addEventListener("DOMContentLoaded", async function renderEvent() {
+document.addEventListener('DOMContentLoaded', async function renderEvent() {
   try {
     const reviewsData = await fetchData();
     renderSlides(reviewsData);
@@ -52,41 +52,41 @@ function renderSlides(reviewsData) {
 
 function initSwiper() {
   const swiper = new Swiper('.swiper-container', {
-  direction: 'horizontal',
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-  mousewheel: {
-    enabled: true,
-    sensitivity: 1,
-    eventsTarget: '.swiper',
-  },
-  navigation: {
-      nextEl: '#next_btn', 
+    direction: 'horizontal',
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+    mousewheel: {
+      enabled: true,
+      sensitivity: 1,
+      eventsTarget: '.swiper',
+    },
+    navigation: {
+      nextEl: '#next_btn',
       prevEl: '#before_btn',
       clickable: true,
     },
-  breakpoints: {
-    375: {
-        slidesPerView: 1
-        },
-    768: {
-        slidesPerView: 2
+    breakpoints: {
+      375: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1440: {
+        slidesPerView: 4,
+      },
     },
-    1440: {
-        slidesPerView: 4
-         }
-    },
-  effect: 'slide',
-  slideToClickedSlide: true,
-  touchRatio: 3,
-  allowTouchMove: true,
+    effect: 'slide',
+    slideToClickedSlide: true,
+    touchRatio: 3,
+    allowTouchMove: true,
   });
-  
-  const sliderNext = document.querySelector("#next_btn");
-  const sliderPrev = document.querySelector("#before_btn");
+
+  const sliderNext = document.querySelector('#next_btn');
+  const sliderPrev = document.querySelector('#before_btn');
 
   sliderNext.addEventListener('click', () => {
     swiper.slideNext(500);
@@ -95,19 +95,4 @@ function initSwiper() {
   sliderPrev.addEventListener('click', () => {
     swiper.slidePrev(500);
   });
-
-  // function updateNavButtons() {
-  //   if (swiper.isBeginning) {
-  //       sliderPrev.disabled = true;
-  //   } else {
-  //       sliderPrev.disabled = false;
-  //   }
-
-  //   if (swiper.isEnd) {
-  //       sliderNext.disabled = true;
-  //   } else {
-  //       sliderNext.disabled = false;
-  //   }
-  // }
-  // updateNavButtons();
 }
